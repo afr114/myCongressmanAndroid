5c4203fd00515122eadc27ec02ff2954ba9920f5
Abdul Rahimi <afr114@gmail.com>
2015-12-08 18:37:45 -0800

    update front end styling

---
+++ app/src/main/java/com/example/guest/mycongressman/Congressman.java
@@ -0,0 +1,34 @@
+package com.example.guest.mycongressman;
+
+/**
+ * Created by Guest on 12/8/15.
+ */
+public class Congressman {
+    public String mFirstName;
+    public String mLastName;
+    public String mChamber;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+    }
+
+    public String getChamber() {
+        return mChamber;
+    }
+
+    public void setChamber(String chamber) {
+        mChamber = chamber;
+    }
+}
---
+++ app/src/main/java/com/example/guest/mycongressman/ui/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.guest.mycongressman.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.example.guest.mycongressman.Congressman;
+import com.example.guest.mycongressman.R;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.zipCodeLabel) EditText mZipCodeLabel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.submitButton)
+    public void switchLayout() {
+        if (mZipCodeLabel != null && !mZipCodeLabel.getText().toString().isEmpty()) {
+            setContentView(R.layout.activity_congressman);
+            String apiKey = "bd43ab92ccfa4c1bb8047f654965f7e9";
+            String sunlightUrl = "https://congress.api.sunlightfoundation.com/legislators/locate?zip=" + mZipCodeLabel.getText().toString() + "&apikey=" + apiKey;
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(sunlightUrl).build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    String jsonData = response.body().string();
+
+                    try {
+                        JSONObject congressInfo = new JSONObject(jsonData);
+                        JSONArray results = congressInfo.getJSONArray("results");
+
+                        Congressman[] congressmans = new Congressman[results.length()];
+
+                        for (int i = 0; i < results.length(); i++) {
+                            JSONObject jsonCongressman = results.getJSONObject(i);
+                            Congressman congressman = new Congressman();
+
+                            congressman.setChamber(jsonCongressman.getString("chamber"));
+                            congressman.setFirstName(jsonCongressman.getString("first_name"));
+                            congressman.setLastName(jsonCongressman.getString("last_name"));
+
+                            congressmans[i] = congressman;
+                            String s = "s";
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+        }
+    }
+
+}
